3895c14f08f0fedf796a4357288e01e3338d26b2
Slocx <33150302+Slocx@users.noreply.github.com>
2022-02-03 11:44:21 +0100

    :tada: initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './pages/Home';
+
+const Stack = createNativeStackNavigator();
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ pages/Home.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <View>
+      <Text> Ceci est la page Home</Text>
+    </View>
+  );
+};
+
+export default Home;
